48e6bef25944c9f276f31c0029f484a334a78e7b
Kenan Öztürk <kenan.ozturk3@hotmail.com>
2021-11-20 18:13:24 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product succesfully has been added";
+        public static string ProductDeleted = "Product succesfully has been deleted";
+        public static string ProductUpdated = "Product succesfully has been updated";
+
+        public static string CategoryAdded = "Category succesfully has been added";
+        public static string CategoryDeleted = "Category succesfully has been deleted";
+        public static string CategoryUpdated = "Category succesfully has been updated";
+
+        public static string UserNotFound = "User Not Found!";
+        public static string PasswordError = "Password Error!";
+        public static string SuccessfulLogin = "Successfully login";
+        public static string UserAlreadyExists = "User Already Exists";
+        public static string UserRegistered = "User successfully has been registered";
+        public static string AccessTokenCreated = "Access Token successfully has been created";
+
+        
+
+
+
+
+
+
+
+
+    }
+}
